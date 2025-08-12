e310b324cc3fa4b2e7aacca806b16b4c465ffbae
johancjacob <johanjacob21@gmail.com>
2025-08-12 18:42:03 +0530

    modified dto

--- RetailStoreInventoryManagementProject/src/com/litmus7/retailstore/dto/Clothing.java
+++ RetailStoreInventoryManagementProject/src/com/litmus7/retailstore/dto/Clothing.java
@@ -5,4 +5,14 @@ public class Clothing extends Product{
 	
 	public String size;
 	public String material;
+	
+	public Clothing(String id,String name,String category,double price,String status,String size,String material) {
+		this.productId=id;
+		this.productName=name;
+		this.category=category;
+		this.price=price;
+		this.status=status;
+		this.size=size;
+		this.material=material;
+	}
 }
