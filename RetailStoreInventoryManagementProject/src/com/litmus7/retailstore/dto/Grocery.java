3e9c5ac1eabcce48ee2aabc22a4bbb6d9ca25120
johancjacob <johanjacob21@gmail.com>
2025-08-12 18:40:37 +0530

    modified dto

--- RetailStoreInventoryManagementProject/src/com/litmus7/retailstore/dto/Grocery.java
+++ RetailStoreInventoryManagementProject/src/com/litmus7/retailstore/dto/Grocery.java
@@ -6,4 +6,14 @@ public class Grocery extends Product{
 	
 	public Date expiryDate;
 	public double weightKg;
+	
+	public Grocery(String id,String name,String category,double price,String status,Date expiryDate,double weightKg) {
+		this.productId=id;
+		this.productName=name;
+		this.category=category;
+		this.price=price;
+		this.status=status;
+		this.expiryDate=expiryDate;
+		this.weightKg=weightKg;
+	}
 }
