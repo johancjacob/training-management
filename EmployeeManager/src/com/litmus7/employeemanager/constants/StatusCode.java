7781901323ce0cdf1e547a47bcf818efe8064696
johancjacob <johanjacob21@gmail.com>
2025-08-30 23:30:59 +0530

    done some edits

--- EmployeeManager/src/com/litmus7/employeemanager/constants/StatusCode.java
+++
@@ -1,8 +0,0 @@
-package com.litmus7.employeemanager.constants;
-
-public class StatusCode {
-	public static final int SUCCESS=200;
-	public static final int PARTIAL_SUCCESS=206;
-	public static final int FAILURE=400;
-
-}
