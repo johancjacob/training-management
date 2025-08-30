c0f6dfdf43fa2e73ccdedf481e1780d03987315c
johancjacob <johanjacob21@gmail.com>
2025-08-30 23:37:53 +0530

    done some edits

--- InventoryFeedProject/src/com/litmus7/inventoryfeed/constants/FilePaths.java
+++
@@ -1,7 +0,0 @@
-package com.litmus7.inventoryfeed.constants;
-
-public class FilePaths {
-	public static String INPUT_FOLDER_PATH="inventory-feed/input";
-	public static String ERROR_FOLDER_PATH="inventory-feed/error";
-	public static String PROCESSED_FOLDER_PATH="inventory-feed/processed";
-}
--- InventoryFeedProject/src/com/litmus7/inventoryfeed/dao/SqlConstants.java
+++
@@ -1,6 +0,0 @@
-package com.litmus7.inventoryfeed.dao;
-
-public class SqlConstants {
-	public static final String INSERT_PRODUCT_QUERY="insert into product(sku,product_name,quantity,price) values(?,?,?,?)";
-
-}
